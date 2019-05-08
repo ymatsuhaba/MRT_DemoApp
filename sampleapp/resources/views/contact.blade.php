bc8be1ca0ceadbab9c394a9c63fb7eb39decf8a0
shoheiseguchi <shohei-seguchi0603@outlook.jp>
2019-05-08 17:56:11 +0900

    contact.blade.phpの編集

--- sampleapp/resources/views/contact.blade.php
+++ sampleapp/resources/views/contact.blade.php
@@ -7,26 +7,43 @@
     <title>フォーム</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
 </head>
-<body>
-    <div class="container">
-    <form>
-        <div class="form-group">
-            <label>Full Name: </label>
-            <input type="text" class="form-control" name="name">
+    <body>
+        <div class="container">
+        <h1>Contact Us</h1>
+
+        <div class="row">
+        <div class="col-md-6">
+        <form method="post" action="{{ route('contact.store') }}">
+        {{ csrf_field() }}
+
+            <div class="form-group">
+                <label>Full Name: </label>
+                <input type="text" class="form-control" name="name">
+                @if ($errors->has('name'))
+                <small class="form-text invalid-feedback">{{ $errors->first('name') }}</small>
+                @endif
+            </div>
+
+            <div class="form-group">
+                    <label>Email Address: </label>
+                    <input type="text" class="form-control" name="email">
+                    @if ($errors->has('email'))
+                    <small class="form-text invalid-feedback">{{ $errors->first('email') }}</small>
+                @endif
+                </div>
+
+            <div class="form-group">
+                    <label>Message: </label>
+                    <textarea name="Message" class="form-control"></textarea>
+                    @if ($errors->has('message'))
+                    <small class="form-text invalid-feedback">{{ $errors->first('message') }}</small>
+                @endif
+                </div>
+
+                <button class="btn btn-primary">Submit</button>
+        </form> 
         </div>
-
-        <div class="form-group">
-                <label>Email Address: </label>
-                <input type="text" class="form-control" name="email">
-            </div>
-
-        <div class="form-group">
-                <label>Message: </label>
-                <textarea name="Message" class="form-control"></textarea>
-            </div>
-
-            <button class="btn btn-primary">Submit</button>
-    </form> 
-</div>
-</body>
+        </div>
+    </div>
+    </body>
 </html>
