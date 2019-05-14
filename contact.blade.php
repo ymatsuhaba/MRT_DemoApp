91e15abf1f7f65536ace6fe99b2b3092f2e40584
sseguchi <sseguchi@ntpc213.medrt.local>
2019-05-14 14:38:00 +0900

    Contact Us に変更

--- contact.blade.php
+++ contact.blade.php
@@ -14,7 +14,7 @@
     </head>
     <body>
         <div class="container">
-            <h1>contact us</h1>
+            <h1>Contact Us</h1>
 
             <div class="row">
                 <div class="col-md-6">
