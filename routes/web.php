844388119a438f7980ed86a92dd702901f13414d
sseguchi <shohei-seguchi0603@outlook.jp>
2019-05-15 14:08:57 +0900

    about.blade.phpの編集

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contact', [
+    'uses' => 'ContactMessageController@create'
+]);
+
+Route::post('/form',function(){
+    return view('form');
+});
+
+Route::get('/login',function(){
+    return view('login');
+});
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
