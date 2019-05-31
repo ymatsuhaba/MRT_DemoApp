030aebb8597c7d9674c78dae914fa5089db7fa62
page32burn <page32burn5syu@gamil.com>
2019-05-31 12:52:07 +0900

    変更

--- resources/views/layouts/head_common.blade.php
+++ resources/views/layouts/head_common.blade.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewpoint" content="width=device-with, initial-scale-1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>ログイン</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <script type="text/javascript"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js"></script>
 </head>
