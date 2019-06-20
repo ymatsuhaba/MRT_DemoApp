3f5e96732b14d4e0860600ac53ef15fb8b998647
sseguchi <shohei-seguchi0603@outlook.jp>
2019-06-20 18:57:07 +0900

    計算履歴一覧表の表示

---
+++ app/Http/Controllers/HistoryController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\History;
+use Illuminate\Database\Eloquent\Collection;
+
+
+class HistoryController extends Controller
+{
+    public function index(){
+        //DBからレコードのデータを取得
+        $all_data = History::all();
+        return view('/history',['history'=>$all_data]);
+    }
+
+    public function plus(){
+        //DBからoperetorカラムが+のレコードのデータ全てを取得
+
+        $all_data = History::all();
+        $history_plus =null;
+        $result = new Collection();
+
+        foreach ($all_data as $history_plus) {
+            if ($history_plus->isplus('+')){
+                $result ->push($history_plus);
+            }
+        }
+        return view('/history_plus',compact('result'));
+    }
+
+
+    public function minus(){
+        //DBからoperetorカラムが-のレコードのデータ全てを取得
+        $all_data = History::all();
+        $history_minus =null;
+        $result = new Collection();
+
+        foreach ($all_data as $history_minus) {
+            if ($history_minus->isminus()){
+                $result ->push($history_minus);
+
+            }
+        }
+        return view('/history_minus',compact('result'));
+    }
+
+    public function time()
+    {
+        $all_data = History::all();
+        $history_time = null;
+        $result = new Collection();
+
+        foreach ($all_data as $history_time) {
+            if ($history_time->istime()){
+                $result ->push($history_time);
+            }
+        }
+        return view('/history_time', compact('result'));
+    }
+
+    public function divide(){
+        $all_data = History::all();
+        $history_divide =null;
+        $result = new Collection();
+
+        foreach ($all_data as $history_divide) {
+            if ($history_divide->isdivide()){
+                $result ->push($history_divide);
+            }
+        }
+        return view('/history_divide',compact('result'));
+    }
+
+    private function kyotu () {
+
+    }
+};
---
+++ resources/views/history_plus.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang=ja>
+<head>
+    <meta charset="UTF-8">
+    <title>計算履歴一覧</title>
+    <style>
+        *{text-align: center; font-family: 'Avenir','Helvetica Neue','Helvetica','Arial','Hiragino Sans','ヒラギノ角ゴシック',YuGothic,'Yu Gothic','メイリオ', Meiryo,'ＭＳ Ｐゴシック','MS PGothic'}
+    </style>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+<header>
+    <hr><h1>演算子が＋のレコード</h1><hr>
+</header>
+<table class="table table-bordered">
+        <tr>
+            <th>数字１</th>
+            <th>演算子</th>
+            <th>数字２</th>
+            <th>計算結果</th>
+        </tr>
+        @foreach($result as $history_plus)
+            <tr>
+                <td>{{$history_plus->number1}}</td>
+                <td>{{$history_plus->operator}}</td>
+                <td>{{$history_plus->number2}}</td>
+                <td>{{$history_plus->result}}</td>
+            </tr>
+        @endforeach
+    </table>
+</body>
+</html>
