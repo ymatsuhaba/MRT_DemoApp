52eccfebe2fa0c8819e5c406db88ce58d21bb344
page32burn <page32burn5syu@gamil.com>
2019-07-25 17:54:29 +0900

    変数名の変更

--- app/Http/Controllers/MRTdbSearchController.php
+++ app/Http/Controllers/MRTdbSearchController.php
@@ -20,43 +20,43 @@ class MRTdbSearchController extends Controller
         $startDate = $request->input('startDate');
 
 
-        $query = DB::table('mrt_dbs');
+        $dataFromDB = DB::table('mrt_dbs');
 
 
         if (isset($prefecture)){
-            $query->where('prefecture',$prefecture);
+            $dataFromDB->where('prefecture',$prefecture);
         }
 
         if (isset($medical)){
-            $query->where('medical',$medical);
+            $dataFromDB->where('medical',$medical);
         }
 
 
         if (isset($workFormDuty) || isset($workFormDayDuty)){
             // 当直が選択されている場合（日直・日当直が選択れていない）
             if (isset($workFormDuty) && !isset($workFormDayDuty)) {
-                $query -> where('workForm', $workFormDuty);
+                $dataFromDB -> where('workForm', $workFormDuty);
             }
             // 日直・日当直が選択されている場合（当直が選択されていない）
             if (!isset($workFormDuty) && isset($workFormDayDuty)) {
-                $query -> where('workForm', $workFormDayDuty);
+                $dataFromDB -> where('workForm', $workFormDayDuty);
             }
         }
 
         if (isset($hourlySalary)) {
-                $query -> where('hourlySalary','>=', $hourlySalary);
-            }
+            $dataFromDB -> where('hourlySalary','>=', $hourlySalary);
+        }
 
 
         if (isset($salary)) {
-            $query -> where('salary','>=', $salary);
+            $dataFromDB -> where('salary','>=', $salary);
         }
 
         if (isset($startDate)){
-            $query->where('startDate',$startDate);
+            $dataFromDB->where('startDate',$startDate);
         }
 
-        $mrtDbCollection = $query->orderby('startDate','asc')->get();
+        $mrtDbCollection = $dataFromDB->orderby('startDate','asc')->get();
 
         return view('mrt-db.second.resultSearch',compact('mrtDbCollection'));
 
