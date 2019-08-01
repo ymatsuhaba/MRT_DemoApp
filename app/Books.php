3c446c17b6a2e44feca50fd101130f1fc34c16b2
sseguchi <shohei-seguchi0603@outlook.jp>
2019-08-01 18:40:18 +0900

    BookRoomの作成
    インデントの修正
    無駄なコメントの削除、修正

--- app/Books.php
+++ app/Books.php
@@ -14,7 +14,7 @@ class Books extends Model
 {
     public $sortable = ['title', 'author','release_date','status','created_at'];    // ソート対象カラム追加
 
-        //    一覧ページ
+    //    一覧ページ
     public function list (Request $request) {
 
         $title = $request->input('title');
@@ -24,9 +24,7 @@ class Books extends Model
         $status_2 = $request->input('status_2');
         $status_3 = $request->input('status_3');
 
-
         $books = DB::table('books');
-
 
         if (isset($title)) {
             $books->where('title', $title);
@@ -53,17 +51,17 @@ class Books extends Model
                 $books->where('status', $status_3);
             }
 
-            // 「貸出中」が選択されていた場合
+            // 「貸出中」「在庫あり」が選択されていた場合
             if (isset($status_1) && isset($status_2) && !isset($status_3)) {
                 $books->where('status', $status_1);
                 $books->orwhere('status', $status_2);
             }
-            // 「在庫あり」が選択されていた場合
+            // 「在庫あり」「紛失中」が選択されていた場合
             if (isset($status_1) && !isset($status_2) && isset($status_3)) {
                 $books->where('status', $status_1);
                 $books->orwhere('status', $status_3);
             }
-            // 「紛失中」が選択されていた場合
+            // 「紛失中」「在庫あり」が選択されていた場合
             if (!isset($status_1) && isset($status_2) && isset($status_3)) {
                 $books->where('status', $status_2);
                 $books->orwhere('status', $status_3);
--- resources/views/BookRoom/search.blade.php
+++ resources/views/BookRoom/search.blade.php
@@ -12,44 +12,38 @@
 <body>
 {{--ヘッダー--}}
 @include('BookRoom.header')
-
-    <div class="row">
-        <div class="col-md-6">
-            <form method="get" action="{{ url('/bookRoom') }}">
-                @csrf
-
-                <div class="form-group">
-                    <label>タイトル: </label>
-                    <input type="text" class="form-control" name="title">
-
-
-                    <label>著者: </label>
-                    <input type="text" class="form-control" name="author">
-
-                    <label>発売日:</label>
-                    <input type="text" class="form-control" name="release_date">
-
-                    <div class="pref">
-                        <div class="decorate-parts"></div>
-                        <div>&ensp;貸出フラグ：&ensp;</div>
-                        <div>
-                            <input type="checkbox" name="status_1" value="1">
-                            <label>&ensp;貸出中&ensp;</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" name="status_2" value="2">
-                            <label>&ensp;在庫あり&ensp;</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" name="status_3" value="3">
-                            <label>&ensp;紛失中&ensp;</label>
-                        </div>
+<div class="row">
+    <div class="col-md-6">
+        <form method="get" action="{{ url('/bookRoom') }}">
+            @csrf
+            <div class="form-group">
+                <label>タイトル: </label>
+                <input type="text" class="form-control" name="title">
+                <label>著者: </label>
+                <input type="text" class="form-control" name="author">
+                <label>発売日:</label>
+                <input type="text" class="form-control" name="release_date">
+                <div class="pref">
+                    <div class="decorate-parts"></div>
+                    <div>&ensp;貸出フラグ：&ensp;</div>
+                    <div>
+                        <input type="checkbox" name="status_1" value="1">
+                        <label>&ensp;貸出中&ensp;</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="status_2" value="2">
+                        <label>&ensp;在庫あり&ensp;</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="status_3" value="3">
+                        <label>&ensp;紛失中&ensp;</label>
                     </div>
                 </div>
-                <button class="btn btn-primary">検索</button>
-            </form>
-        </div>
+            </div>
+            <button class="btn btn-primary">検索</button>
+        </form>
     </div>
+</div>
 </div>
 <!-- フッター -->
 @include('BookRoom.footer')
