e3657e25a3118b624bb2076cbce8a9b37c643880
page32burn <page32burn5syu@gamil.com>
2019-08-01 18:45:21 +0900

    変更点の修正 exit git commit -m 変更点の修正

--- app/Http/Controllers/yukiLabController.php
+++ app/Http/Controllers/yukiLabController.php
@@ -219,14 +219,4 @@ class yukiLabController extends Controller
 
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
