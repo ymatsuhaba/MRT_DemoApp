4edbc2afda36c90df21380f1e4f059e357b84f9b
Miyashige Yukikane <miyashigeyukikane@MiyashigenoMacBook-Air.local>
2019-08-03 15:51:39 +0900

    pictures

--- resources/views/SpMrt/head.blade.php
+++ resources/views/SpMrt/head.blade.php
@@ -7,6 +7,8 @@
         <meta content="text/javascript" http-equiv="Content-Type">
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!--font Awesomeの使用-->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous"><link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
         <!--　CSSの適応-->
         <link rel="stylesheet" type="text/css" href="css/SpMrt/reset.css">
         <link rel="stylesheet" type="text/css" href="css/SpMrt/header.css">
--- resources/views/SpMrt/index.blade.php
+++ resources/views/SpMrt/index.blade.php
@@ -6,7 +6,9 @@
 
 <body>
     <div id="picture">
-
+        <img src="SpMrt_png/main1.png" class="photo" alt="main1">
+{{--        <img src="SpMrt_png/main2.png" class="photo" alt="main2">--}}
+{{--        <img src="SpMrt_png/main3.png" class="photo" alt="main3">--}}
     </div>
 
     <div id="search">
