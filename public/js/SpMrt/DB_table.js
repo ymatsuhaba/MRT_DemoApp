06bfb5bf88c9a5daf7f9bd8c89908bd57ee83621
page32burn <page32burn5syu@gamil.com>
2019-08-08 18:32:03 +0900

    いらないコメントアウトの削除等

--- public/js/SpMrt/DB_table.js
+++ public/js/SpMrt/DB_table.js
@@ -1,16 +1,12 @@
 $(function() {
-
     $(".ID").mouseover(function() {
 
         $(this).animate({fontSize : '4vh'}, 500);
         $(this).css('color','blue');
 
     }).mouseout(function() {
-
         $(this).animate({fontSize : '2vh'}, 500);
         $(this).css('color','black');
-
     });
-
 });
 
--- public/js/SpMrt/index.js
+++ public/js/SpMrt/index.js
@@ -2,17 +2,14 @@ function link() {
       alert('新規登録画面に移動しますか?');
 }
 
-
 //iconのbackground-colorをredにする
 $(function(){
-      $(".main").hover(
-          function() {
-                //マウスカーソルが重なった時の処理
-                $(".main").css('background-color', 'blue');
-          },
-          function() {
-                //マウスカーソルが離れた時の処理
-                $(".main").css('background-color', 'black');
-          }
-      );
+    $(".main").hover(
+        function() {
+        $(".main").css('background-color', 'blue');
+        },
+        function() {
+        $(".main").css('background-color', 'black');
+        }
+    );
 });
